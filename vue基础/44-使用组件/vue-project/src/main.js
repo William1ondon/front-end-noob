c2ea4f4180887317f187d0efdac93da94084999b
William London <wl1959479486@icloud.com>
2023-06-16 00:35:57 +0800

    2023-6-16 good night.

---
+++ vue基础/44-使用组件/vue-project/src/main.js
@@ -0,0 +1,7 @@
+// import './assets/main.css'
+
+import { createApp } from 'vue'
+// import App from './App.vue'
+import ButtonCounter from './components/ButtonCounter.vue'
+
+createApp(ButtonCounter).mount('#app')
